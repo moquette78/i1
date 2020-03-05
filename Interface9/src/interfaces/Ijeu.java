fbf20ce36a4bf2ce6ede02153d665c625021ab31
Giovanni SIMON <giovanni.simon58@gmail.com>
2020-03-05 15:22:12 +0100

    ok

--- Interface9/src/interfaces/Ijeu.java
+++ Interface9/src/interfaces/Ijeu.java
@@ -4,5 +4,12 @@ package interfaces;
 public interface Ijeu {
 	
 	abstract void jouer();
+	default void gagner() {
+			System.out.print("ok");
+	}	
+	
+	//ne marche pas car interface fonctionnelle donc une seul methode abstraite
+	
+	//abstract void perdre();
 
 }
